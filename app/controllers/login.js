ee4956ee0d64ac82d21cf581213d8f094defec57
Josué Umaña Guzmán <jrug_89@hotmail.com>
2018-06-16 13:25:44 -0600

    [CM-009@C], session control, login control
    session control, login control

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -5,8 +5,21 @@ export default Controller.extend({
     actions:{
         authenticate(){
             let { email, password } = this.getProperties('email', 'password');
-            console.log(email);
-            console.log(password);
+            
+            $.getJSON( "/data/users.json", function(data) {
+              })
+                .done(function(data) {
+                    if(typeof(data[email]!='undefined')){
+                        if(data[email]==password){
+                            console.log('LoginSucces');
+                            sessionStorage.setItem("user", email);
+                            window.location.replace("/content");
+                        }
+                    }
+                })
+                .fail(function() {
+                  console.log( "Sorry, we have an error with the server" );
+                }) 
         } 
     }
 });
--- app/routes/content.js
+++ app/routes/content.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    beforeModel(){
+        if (sessionStorage.getItem("user") === null) {
+            this.transitionTo('login');
+          }
+    }
 });
