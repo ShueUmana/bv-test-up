5ad49a53c84f61d1a26b1d60c6428c6ec4eb1853
Josué Umaña Guzmán <jrug_89@hotmail.com>
2018-06-17 16:36:44 -0600

    [CM-015@A], Adding controllers to store response
    Adding controllers to store response  in LocalStorage, response History

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -6,6 +6,28 @@ export default Controller.extend({
             // control para borrado de la session,
             sessionStorage.clear();
             window.location.replace("/");
+        },
+        searchMailData(){
+            // get data from form
+            let { email } = this.getProperties('email');
+            // post data to REST listener
+            $.post( "http://www.beenverified.com/hk/dd/email?email="+email, function(data) {})
+                .done(function(data) {
+                    // function to store data in localStorage 
+                    if(localStorage.getItem("SearchHistoryBVTest") !== null){
+                        // add data to localStorage,
+                        let searchhistory =JSON.parse(localStorage.getItem('SearchHistoryBVTest'));
+                        searchhistory.push(data);
+                        localStorage.setItem("SearchHistoryBVTest", JSON.stringify(searchhistory));
+                    }else{  // first entry is empty, Creation
+                        console.log('else')
+                        let firstObj=[data];
+                        localStorage.setItem("SearchHistoryBVTest", JSON.stringify(firstObj));
+                    }
+                })
+                .fail(function() {
+                  alert( "ups, access-control-allow-origin, fix this option first" );
+                })
         }
     }
 });
