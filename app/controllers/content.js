88df1b02e3310d67a9262b14d8ce530b94988063
Josué Umaña Guzmán <jrug_89@hotmail.com>
2018-06-17 23:56:55 -0600

    [CM-022@A], ajuste ajax, cambio del $.get
     ajuste ajax, cambio del $.get

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -1,33 +1,44 @@
 import Controller from '@ember/controller';
 import $ from 'jquery';
 export default Controller.extend({
-    actions:{
-        logout(){
+    actions: {
+        logout() {
             // control para borrado de la session,
             sessionStorage.clear();
             window.location.replace("/");
         },
-        searchMailData(){
+        searchMailData() {
             // get data from form
             let { email } = this.getProperties('email');
-            // post data to REST listener
-            $.post( "http://www.beenverified.com/hk/dd/email?email="+email, function() {})
-                .done(function(data) {
+
+            $.ajax({
+                url: "http://www.beenverified.com/hk/dd/email?email=" + email,
+                type: 'GET',
+                crossDomain: true,
+                dataType: 'jsonp',
+                success: function (data) {
                     // function to store data in localStorage 
-                    if(localStorage.getItem("SearchHistoryBVTest") !== null){
+                    if (localStorage.getItem("SearchHistoryBVTest") !== null) {
                         // add data to localStorage,
-                        let searchhistory =JSON.parse(localStorage.getItem('SearchHistoryBVTest'));
+                        let searchhistory = JSON.parse(localStorage.getItem('SearchHistoryBVTest'));
                         searchhistory.push(data);
                         localStorage.setItem("SearchHistoryBVTest", JSON.stringify(searchhistory));
-                    }else{  // first entry is empty, Creation
-                        let firstObj=[data];
+                    } else {  // first entry is empty, Creation
+                        let firstObj = [data];
                         localStorage.setItem("SearchHistoryBVTest", JSON.stringify(firstObj));
                     }
-                    window.location.replace("/response");  
-                })
-                .fail(function() {
-                  alert( "ups, access-control-allow-origin, fix this option first" );
-                })
+                    window.location.replace("/response");
+                }
+            });
+            /*
+                        // post data to REST listener
+                        $.post( "http://www.beenverified.com/hk/dd/email?email="+email, function() {})
+                            .done(function(data) {
+                                
+                            })
+                            .fail(function() {
+                              alert( "ups, access-control-allow-origin, fix this option first" );
+                            })*/
         }
     }
 });
