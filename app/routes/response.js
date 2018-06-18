dc68a23b61418d11512378b7512b0035fdc5a427
Josué Umaña Guzmán <jrug_89@hotmail.com>
2018-06-17 21:10:48 -0600

    [CM-019@T], ajuste en caso de vacio final test
    ajuste en caso de vacio final test

--- app/routes/response.js
+++ app/routes/response.js
@@ -4,7 +4,7 @@ export default Route.extend({
     model(params){
         let searchhistory =JSON.parse(localStorage.getItem('SearchHistoryBVTest'));
         let pos=(searchhistory.length)-1;
-        if(typeof(params)!='undefined'){
+        if(typeof(params.id)!='undefined'){
             pos=params.id;
         }        
         return searchhistory[pos];
